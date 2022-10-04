c55bd4db376f5e738b7523d52077f2b9c2eff05c
Joseph Kim <kimg009263@gmail.com>
2022-10-05 10:21:57 +1300

    heroku deployment

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 // app.js
-
+require("dotenv").config({ path: ".env"});
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('Hello world!'));
 
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("Api running");
+    })
+}
+
+app.get('/', (req, res) => res.send('Hello world!'));
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
